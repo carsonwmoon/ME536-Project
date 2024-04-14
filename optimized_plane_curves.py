446e573317abd566bf75b527f0c3f02df7e581ae
moondude12 <moondude12@gmail.com>
2024-04-13 22:59:34 -0600

    This is working pretty well now

--- optimized_plane_curves.py
+++ optimized_plane_curves.py
@@ -4,148 +4,212 @@ import matplotlib.pyplot as plt
 
 opti = asb.Opti()
 
-phi_max = np.radians(30) # [rad]
-phi_dot_max = np.radians(1) # [rad/s]
-p_init = np.array([0., 0.]) # [m]
-course_init = np.radians(30) # [rad]
-speed = 10. # [m/s]
-g = 9.79768 # [m/s^2]
-t_end_init = 10. # [s]
-N = 100 # [-]
+courses_init = np.radians(np.linspace(0., 90., 4))
+courses_init = np.array([np.radians(20)])
 
-# set initial values
-time_init = np.linspace(0, t_end_init, N)
-phis_init = np.linspace(0., 0., N)
-phis_dot_init = np.linspace(0., 0., N)
-px_init = np.linspace(0, 1, N)
-py_init = np.linspace(0, 0, N) # np.exp(-time_init) * time_init
-vx_init = np.linspace(0, 0, N) # np.exp(-time_init) * (1. - time_init)
-vy_init = np.linspace(speed, speed, N) # np.linspace(1, 1, N)
-ax_init = np.linspace(0, 0, N) # np.linspace(0, 0, N)
-ay_init = np.linspace(0, 0, N) # np.exp(-time_init) * (time_init - 2.)
-courses_init = np.linspace(course_init, 0, N)
+for course_init in courses_init:
 
-# plot the initial values
-# plt.plot(px_init, py_init)
-# plt.axis('equal')
-# plt.show()
-# quit()
+    # course_init = np.radians(90) # [rad]
 
-# define opti variables
-t_end = opti.variable(
-    init_guess=t_end_init, 
-    lower_bound=0
-)
-time = np.linspace(0, t_end, N)
-phis = opti.variable(
-    init_guess=phis_init
-)
-courses = opti.variable(
-    init_guess=courses_init
-)
-vx = np.cos(courses) * speed
-vy = np.sin(courses) * speed
+    phi_max = np.radians(20) # [rad]
+    phi_dot_max = np.radians(360.*1) # [rad/s]
+    p_init = np.array([0., 0.]) # [m]
+    speed = 5. # [m/s]
+    g = 9.79768 # [m/s^2]
+    t_end_init = 1. # [s]
+    N = 250 # [-]
 
-px = opti.variable(
-    init_guess=px_init
-)
-opti.constrain_derivative(
-    variable=px,
-    with_respect_to=time,
-    derivative=vx
-)
-py = opti.variable(
-    init_guess=py_init
-)
-opti.constrain_derivative(
-    variable=py,
-    with_respect_to=time,
-    derivative=vy
-)
+    # just in case
+    phi_max = np.clip(phi_max, -np.radians(90), np.radians(90))
 
-ax = opti.derivative_of(
-    vx,
-    with_respect_to=time,
-    derivative_init_guess=ax_init
-)
-ay = opti.derivative_of(
-    vy,
-    with_respect_to=time,
-    derivative_init_guess=ay_init
-)
-phi_dot = opti.derivative_of(
-    phis,
-    with_respect_to=time,
-    derivative_init_guess=phis_dot_init
-)
+    # set initial values
+    time_init = np.linspace(0, t_end_init, N)
+    phis_init = np.linspace(0., 0., N)
+    phis_dot_init = np.linspace(0., 0., N)
+    px_init = np.linspace(0, t_end_init * speed, N)
+    a, b, c = np.array([3, -4, 1]) * speed * np.sin(course_init)
+    py_init = a*time_init**2 + b*time_init + c # np.linspace(0, 0, N) # np.exp(-time_init) * time_init
+    vx_init = np.linspace(speed*np.cos(course_init), speed, N) # np.exp(-time_init) * (1. - time_init)
+    vy_init = np.linspace(speed*np.sin(course_init), 0, N) # np.linspace(speed, speed, N) # np.linspace(1, 1, N)
+    ax_init = (speed - speed*np.cos(course_init)) / t_end_init * np.ones(N) # np.linspace(0, 0, N)
+    ay_init = (0-speed*np.sin(course_init)) / t_end_init * np.ones(N) # np.exp(-time_init) * (time_init - 2.)
+    courses_init = np.linspace(course_init, 0, N)
 
-opti.subject_to([
-    np.diff(phis) <= phi_dot_max * np.diff(time),
-    np.diff(phis) >= -phi_dot_max * np.diff(time),
-    phis <= phi_max,
-    phis >= -phi_max,
-])
+    # plot the initial values
+    # plt.plot(px_init, py_init)
+    # plt.axis('equal')
+    # plt.show()
+    # quit()
 
-# constrain initial values
-opti.subject_to([
-    px[0] == p_init.item(0),
-    py[0] == p_init.item(1),
-    courses[0] == courses_init[0],
-    vx[0] == np.cos(courses_init[0]) * speed,
-    vy[0] == np.sin(courses_init[0]) * speed,
-    phis[0] == phis_init[0],
-])
+    # define opti variables
+    t_end = opti.variable(
+        init_guess=t_end_init, 
+        lower_bound=0.,
+        category='t_end'
+    )
+    time = np.linspace(0, t_end, N)
+    phis = opti.variable(
+        init_guess=phis_init,
+        category='phis'
+    )
+    courses = opti.variable(
+        init_guess=courses_init,
+        category='courses'
+    )
+    vx = np.cos(courses) * speed
+    vy = np.sin(courses) * speed
 
-# constrain final values
-opti.subject_to([
-    py[-1] == 0, # final y position is 0
-    vy[-1] == 0, # final y velocity is 0
-    courses[-1] == 0, # final course is 0
-    phis[-1] == 0, # final phi is 0
-])
+    px = opti.variable(
+        init_guess=px_init
+    )
+    opti.constrain_derivative(
+        variable=px,
+        with_respect_to=time,
+        derivative=vx,
+    )
+    py = opti.variable(
+        init_guess=py_init
+    )
+    opti.constrain_derivative(
+        variable=py,
+        with_respect_to=time,
+        derivative=vy,
+    )
 
-# constrain dynamics follow a plane turning model
-omega = opti.derivative_of(
-    courses,
-    with_respect_to=time,
-    derivative_init_guess=courses_init
-)
-opti.subject_to([
-    omega == g * np.tan(phis),
-])
+    ax = opti.derivative_of(
+        vx,
+        with_respect_to=time,
+        derivative_init_guess=ax_init,
+    )
+    ay = opti.derivative_of(
+        vy,
+        with_respect_to=time,
+        derivative_init_guess=ay_init,
+    )
+    phis_dot = opti.derivative_of(
+        phis,
+        with_respect_to=time,
+        derivative_init_guess=phis_dot_init,
+        method='forward euler'
+    )
+
+    opti.subject_to([
+        phis_dot <= phi_dot_max,
+        phis_dot >= -phi_dot_max,
+        phis < phi_max,
+        phis > -phi_max,
+    ])
+
+    # constrain initial values
+    opti.subject_to([
+        px[0] == p_init.item(0),
+        py[0] == p_init.item(1),
+        courses[0] == courses_init[0],
+        vx[0] == np.cos(courses_init[0]) * speed,
+        vy[0] == np.sin(courses_init[0]) * speed,
+    ])
+
+    # constrain final values
+    opti.subject_to([
+        py[-1] == 0, # final y position is 0
+        vy[-1] == 0, # final y velocity is 0
+        courses[-1] == 0, # final course is 0
+        phis[-1] == 0, # final phi is 0
+        phis_dot[-1] == 0, # final phi_dot is 0
+    ])
+
+    # constrain dynamics follow a plane turning model
+    omega = opti.derivative_of(
+        courses,
+        with_respect_to=time,
+        derivative_init_guess=g * np.tan(phis_init),
+    )
+    opti.subject_to([
+        omega == g * np.tan(phis),
+    ])
+
+    opti.subject_to([
+        courses <= np.radians(90),
+        courses >= -np.radians(90),
+        py >= 0,
+        np.abs(courses) <= np.abs(courses_init[0])
+    ])
 
 
-# minimize time to reach the final position
-opti.minimize(t_end)
+    # minimize time to reach the final position
+    opti.minimize(t_end)
+    # opti.minimize(np.sum(phis_dot**2) * t_end)
+    # opti.minimize(np.sum(np.diff(phis_dot)**2 * np.diff(time)))
+    # opti.minimize(np.sum(np.trapz(phis_dot ** 2) * np.diff(time)) * t_end)
 
-sol = opti.solve(
-    verbose=True,
-    max_runtime=15.,
-    behavior_on_failure='return_last'
-)
+    sol = opti.solve(
+        verbose=True,
+        max_runtime=15.*4,
+        behavior_on_failure='return_last'
+    )
 
-phis = sol.value(phis)
-px = sol.value(px)
-py = sol.value(py)
-courses = sol.value(courses)
-time = sol.value(time)
+    # design_parameters = {k:sol(opti.variables_categorized[k]) for k in opti.variables_categorized}
+    # print('Design Parameters:')
+    # for k, v in design_parameters.items():
+    #     print(f'{k}: {v}')
+    # quit()
 
-# plot the results
-plt.plot(px, py)
-plt.xlabel('x [m]')
-plt.ylabel('y [m]')
-plt.title('optimized path')
-plt.axis('equal')
-plt.show()
+    # extract the results
+    t_end = sol.value(t_end)
+    time = sol.value(time)
+    phis = sol.value(phis)
+    courses = sol.value(courses)
+    vx = sol.value(vx)
+    vy = sol.value(vy)
+    px = sol.value(px)
+    py = sol.value(py)
+    ax = sol.value(ax)
+    ay = sol.value(ay)
+    phis_dot = sol.value(phis_dot)
+    omega = sol.value(omega)
 
-plt.plot(time, phis*180/np.pi)
-plt.xlabel('time [s]')
-plt.ylabel('phi [rad]')
-plt.title('phi vs time')
-plt.show()
+    # computed values
+    p = np.vstack((px, py))
+    path_length = np.sum(np.linalg.norm(np.diff(p, axis=1), axis=0))
+    status = sol.stats()['success']
 
-plt.plot(time, courses*180/np.pi)
-plt.xlabel('time [s]')
-plt.ylabel('phi [rad]')
-plt.title('phi vs time')
-plt.show()
+
+    # print the results
+    if status:
+        print(f't_end = \n{t_end}')
+        print(f'time = \n{time}')
+        print(f'phis = \n{phis}')
+        print(f'courses = \n{courses}')
+        print(f'vx = \n{vx}')
+        print(f'vy = \n{vy}')
+        print(f'px = \n{px}')
+        print(f'py = \n{py}')
+        print(f'ax = \n{ax}')
+        print(f'ay = \n{ay}')
+        print(f'phis_dot = \n{phis_dot}')
+        print(f'omega = \n{omega}')
+
+    # print important values
+    print(f"Solution Status: {status}")
+    print(f't_end = \n{t_end}')
+    print(f'path_length = \n{path_length}')
+
+    # plot the results
+    if status:
+        plt.plot(px, py, label='Optimized Path')
+    else:
+        plt.plot(px, py, label='Unsuccessful Path')
+    plt.plot(p_init[0], p_init[1], 'ro', label='Initial Position')
+    plt.plot(px_init, py_init, 'k--', label='Initial Path')
+    plt.xlabel('x [m]')
+    plt.ylabel('y [m]')
+    plt.axis('equal')
+    plt.legend()
+    plt.show()
+
+    plt.plot(time, phis*180/np.pi, label='phi')
+    plt.plot(time, phis_dot*180/np.pi, label='phi_dot')
+    plt.plot(time, courses*180/np.pi, label='course')
+    plt.xlabel('time [s]')
+    plt.legend()
+    plt.show()
